d76d99e42e333069921860a5fcca4ebb8f389771
wally_yoon <bluegmlduf2@naver.com>
2023-06-16 23:08:01 +0900

    updae: 카드css수정, 카드더미데이터추가

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,15 +1,15 @@
-<div class="rounded-lg overflow-hidden shadow-lg cursor-pointer hover:grow hover:shadow-lg">
-    <div>
-        <img class="w-full h-250"
-            src="https://images.pexels.com/photos/3861458/pexels-photo-3861458.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-            alt="">
-    </div>
-    <div class="p-4 bg-white flex flex-col min-h-275">
-        <h4 class="text-xl font-bold">Why is the Tesla Cybertruck designed the way it is?</h4>
-        <p class="text-sm my-3">Numquam nemo magni temporibus quo reiciendis minima nostrum voluptas deserunt obcaecati
-            exercitationem?</p>
-        <div>
-            <small class="text-gray-600">2 minutes ago</small>
+{{-- <a href="{{ route('posts.show', ['post' => 1]) }}"> --}}
+<a href="#">
+    <div class="rounded-lg overflow-hidden shadow-lg hover:grow hover:shadow-lg">
+        <div class="w-full h-64">
+            <img class="w-full h-full bg-center" src={{ $post['titleImage'] }} alt="postimage">
+        </div>
+        <div class="p-4 bg-white flex flex-col min-h-275">
+            <h4 class="text-xl font-bold truncate">{{ $post['title'] }}</h4>
+            <p class="text-sm my-3 line-clamp-3">{{ $post['content'] }}</p>
+            <div>
+                <small class="text-gray-600">{{ $post['time'] }}</small>
+            </div>
         </div>
     </div>
-</div>
+</a>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,15 @@
+@php
+    for ($i = 1; $i <= 5; $i++) {
+        $post = [
+            'title' => "게시물 제목 Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, dicta consequuntur! Non eos atque ipsam fugiat ab esse provident temporibus asperiores optio quaerat quibusdam laudantium tempora, vitae vero alias at!{$i}",
+            'content' => "게시물 내용 Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, dicta consequuntur! Non eos atque ipsam fugiat ab esse provident temporibus asperiores optio quaerat quibusdam laudantium tempora, vitae vero alias at!{$i}",
+            'time' => $i,
+            'titleImage' => 'https://picsum.photos/300',
+        ];
+    
+        $posts[] = $post;
+    }
+@endphp
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-bold text-3xl mt-2 mb-4 md:my-7">
@@ -6,10 +18,8 @@
     </x-slot>
 
     <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
-        <x-card/>
-        <x-card/>
-        <x-card/>
-        <x-card/>
-        <x-card/>
+        @foreach ($posts as $post)
+            <x-card :post="$post" />
+        @endforeach
     </div>
 </x-app-layout>
