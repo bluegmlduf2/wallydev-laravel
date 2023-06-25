5484ad4ccde25e870b7cfea58f028e2666e8a747
wally_yoon <bluegmlduf2@naver.com>
2023-06-25 17:09:11 +0900

    add : 상세페이지 표시기능 추가

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="flex flex-col">
         <x-slot name="header">
-            <h1 class="font-bold text-3xl mt-2 mb-4 md:my-7">
+            <h1 class="font-bold text-4xl mt-2 mb-4 md:my-7">
                 {{ $post->title }}
             </h1>
         </x-slot>
@@ -9,6 +9,10 @@
             @php
                 echo $post->content;
             @endphp
+        </div>
+        <div class="flex justify-start mt-5 mb-3 text-gray-500 border-t-2 border-t-gray-300 p-3">
+            <span>{{ \Carbon\Carbon::parse($post->createdDate)->toFormattedDateString() }}</span>
+            <span class="pl-3">Views {{ $post->postViewCount }}</span>
         </div>
     </div>
 </x-app-layout>
