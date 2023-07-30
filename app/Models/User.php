fd035de0d86fa5df7008968e4bf03c786db8a7b0
wally_yoon <bluegmlduf2@naver.com>
2023-07-30 11:49:44 +0900

    fix: 관리자 권한 확인방식변경 (env->db)

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,12 +25,15 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for serialization.
+     * Laravel에서 모델을 JSON으로 반환하거나, 모델을 response()->json() 함수를 사용하여 JSON으로 반환하거나, API 엔드포인트에서 JSON 응답을 생성하는 경우
+     * 해당 칼럼의 값은 숨긴다. 즉 response로 반환하지않는다
      *
      * @var array<int, string>
      */
     protected $hidden = [
         'password',
         'remember_token',
+        'is_admin',
     ];
 
     /**
@@ -40,5 +43,10 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+    ];
+
+    // 칼럼 기본값 설정
+    protected $attributes = [
+        'is_admin' => false,
     ];
 }
