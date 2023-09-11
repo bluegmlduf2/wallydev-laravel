0a83f75c4dd24aed7240ac9364cf617c208c43c9
wally_yoon <bluegmlduf2@naver.com>
2023-09-11 23:28:22 +0900

    add : DB자동 백업 콘솔 추가_3

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,11 +15,15 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // 로컬 실행 명령어 (cron대신에 일시적으로 실행)
+        // 로컬 아래의 커맨드 스케줄 실행 명령어 (cron대신에 일시적으로 실행시 사용)
         // php artisan schedule:work
-        // 서버 실행 명령어
+        
+        // 서버 실행 명령어 아래의 크론을 사용해서 매 분마다 php artisan schedule:run 한다
         // * * * * * cd /home/ubuntu/wallydev-laravel && php artisan schedule:run >> /dev/null 2>&1
+        // 사이트맵 생성 명령어
         $schedule->command('sitemap:generate')->daily();
+        // DB백업 명령어
+        $schedule->command('database:backup')->daily();
     }
 
     /**
