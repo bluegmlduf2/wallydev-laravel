ac763457aa14a049b6b07946cb79b03a40c1d61a
wally_yoon <bluegmlduf2@naver.com>
2023-09-17 15:51:52 +0900

    update : DB자동 백업 _ AWS저장기능추가

--- app/Console/Commands/DatabaseBackup.php
+++ app/Console/Commands/DatabaseBackup.php
@@ -4,7 +4,9 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Carbon\Carbon;
+use Illuminate\Http\File;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseBackup extends Command
 {
@@ -44,8 +46,8 @@ class DatabaseBackup extends Command
         $host = config('database.connections.' . $connection . '.host');
         $database = config('database.connections.' . $connection . '.database');
 
-        $command = "mysqldump -u $user -p'$password' --host $host $database > $filePath";
-        // $command = "mysqldump --login-path=$user --host=$host $database > $filePath";
+        $command = "mysqldump -u $user -p'$password' --host $host $database > $filePath"; // 배포 서버용
+        // $command = "sudo mysqldump -u $user -p$password --host $host $database > $filePath"; // LOCAL 테스트용
 
         $output = null;
         exec($command, $output, $error);
@@ -53,6 +55,17 @@ class DatabaseBackup extends Command
         // exec 를 통해 에러가 발생하면 $error 변수에 에러코드가 담깁니다.
         if (isset($error) && $error > 0) {
             Log::warning("Database Backup Failed. ERROR CODE : " . $error);
+        }
+
+        if (file_exists($filePath)) {
+            // SQL 파일이 존재하면 Illuminate\Http\File 객체로 반환
+            $file = new File($filePath);
+
+            // 이전에 저장한 SQL 파일 삭제 
+            Storage::disk('s3')->deleteDirectory('backup/database');
+
+            // SQL 파일을 Amazon S3에 업로드 (put메서드는 Illuminate\Http\File을 매개변수로 사용)
+            Storage::disk('s3')->put('backup/database', $file);
         }
 
         Log::info("Database backup was successful. File Name :" . $fileName);
