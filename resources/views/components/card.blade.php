25b48919e8d588f09d72a0c50d86aaffa1520937
wally_yoon <bluegmlduf2@naver.com>
2024-03-31 15:45:40 +0900

    update : 표시 시간의 데이터 포맷을 변경

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -8,7 +8,7 @@
             <h4 class="text-xl font-bold truncate">{{ $post['title'] }}</h4>
             <p class="text-sm my-3 line-clamp-3">{{ $post['contentShort'] }}</p>
             <div>
-                <small class="text-gray-600">{{ $post['createdDate'] }}</small>
+                <small class="text-gray-600">{{ date_format($post['createdDate'],"Y-m-d H:i") }}</small>
             </div>
         </div>
     </div>
